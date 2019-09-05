1b9a6225826455cecfb190fd6a4d10b6230afa3a
Hasuuuuu <1notes7bt@gmail.com>
2019-09-05 16:21:25 +0900

    ゲーム中の操作説明の表示非表示可能化

---
+++ 3Rts_Github/Assets/GuideTextUiSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GuideTextUiSystem : MonoBehaviour
+{
+    public GameObject guideText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Back"))
+        {
+            guideText.SetActive(!guideText.activeSelf);
+        }
+    }
+}
