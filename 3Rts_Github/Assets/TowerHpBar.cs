3a853bf9a5d28558ca03686df9db73137754e098
pino_oc <hanaserebu1472@gmail.com>
2019-09-06 02:02:55 +0900

    タワーのHPばーの向き調整

--- 3Rts_Github/Assets/TowerHpBar.cs
+++ 3Rts_Github/Assets/TowerHpBar.cs
@@ -17,6 +17,11 @@ public class TowerHpBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 targetPos = Camera.main.transform.position;
+        // ターゲットのY座標を自分と同じにすることで2次元に制限する。
+        targetPos.y = 0;
+        transform.parent.LookAt(targetPos);
+
         pcr = player.GetComponent<PlayerController>().HPvar;
         if (pcr == true)
         {
