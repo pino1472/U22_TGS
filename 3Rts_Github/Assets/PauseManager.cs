6c88e903fdbbfb8f400d2f6107979ccf47f061cc
Rize0214 <mesitukai1213@gmail.com>
2019-09-12 12:03:27 +0900

    佐渡汽船完成

--- 3Rts_Github/Assets/PauseManager.cs
+++ 3Rts_Github/Assets/PauseManager.cs
@@ -1,31 +1,106 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //[SerializeField] GameObject[] pasue;
+    //[SerializeField] int x;
+    //[SerializeField] Animator aru;
+    //[SerializeField]float time,asy;
+  
     void Start()
     {
-        
+        //x = 0;
+        //for (int y = 0; pasue.Length > y; y++)
+        //{
+        //    aru = pasue[y].GetComponent<Animator>();
+        //    aru.SetBool("aut", false);
+        //}
+        //aru = pasue[x].GetComponent<Animator>();
+        //aru.SetBool("aut", true);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //float har = Input.GetAxis("L Stick H");
+        //time += Time.deltaTime;
+        //if (time > asy)
+        //{
+        //    if (har < 0)
+        //    {
+        //        if (x == pasue.Length - 1)
+        //        {
+        //            for (int y = 0; pasue.Length > y; y++)
+        //            {
+        //                aru = pasue[y].GetComponent<Animator>();
+        //                aru.SetBool("aut", false);
+        //            }
+        //            aru = pasue[x].GetComponent<Animator>();
+        //            aru.SetBool("aut", true);
+        //        }
+        //        else
+        //        {
+        //            x++;
+        //            for (int y = 0; pasue.Length > y; y++)
+        //            {
+        //                aru = pasue[y].GetComponent<Animator>();
+        //                aru.SetBool("aut", false);
+        //            }
+        //            aru = pasue[x].GetComponent<Animator>();
+        //            aru.SetBool("aut", true);
+        //        }
+        //        time = 0;
+        //    }
+        //    if (har > 0)
+        //    {
+        //        if (x == 0)
+        //        {
+        //            x = pasue.Length - 1;
+        //            for (int y = 0; pasue.Length > y; y++)
+        //            {
+        //                aru = pasue[y].GetComponent<Animator>();
+        //                aru.SetBool("aut", false);
+        //            }
+        //            aru = pasue[x].GetComponent<Animator>();
+        //            aru.SetBool("aut", true);
+        //        }
+        //        else
+        //        {
+        //            x--;
+        //            for (int y = 0; pasue.Length > y; y++)
+        //            {
+        //                aru = pasue[y].GetComponent<Animator>();
+        //                aru.SetBool("aut", false);
+        //            }
+        //            aru = pasue[x].GetComponent<Animator>();
+        //            aru.SetBool("aut", true);
+        //        }
+        //        time = 0;
+        //    }
+        //}
+
+        //if(Input.GetKeyDown("joystick button 0"))
+        //{
+
+        //}
+
+
         if (Input.GetButton("Start")&&Time.timeScale ==1)
         {
-            Time.timeScale = 0;
-
+            Time.timeScale = 0; 
+            
         }
 
-        if (Input.GetButton("Back") && Time.timeScale == 0)
+        if (Input.GetButton("Start") && Time.timeScale == 0)
         {
             Time.timeScale = 1;
         }
 
-
-
     }
-    }
+}
 
--- 3Rts_Github/Assets/PauseSystem.cs
+++ 3Rts_Github/Assets/PauseSystem.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class PauseSystem : MonoBehaviour
 {
-
+    
     public GameObject pauseText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
+
 
     // Update is called once per frame
     void Update()
@@ -21,10 +23,15 @@ public class PauseSystem : MonoBehaviour
         if (Input.GetButton("Start"))
         {
             pauseText.SetActive(true);
+           
+        }
+        if (Input.GetKeyDown("joystick button 0"))
+        {
+            pauseText.SetActive(false);
         }
         if (Input.GetButton("Back"))
         {
-            pauseText.SetActive(false);
+            SceneManager.LoadScene("Startmeny");
         }
     }
 }
